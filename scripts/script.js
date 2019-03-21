d9038c3da8ef95f15ed74df136f8ca5f36649fe4
George <bl1723r@gmail.com>
2019-03-22 01:07:37 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -145,25 +145,6 @@ $(function() {
 
   setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
 
-  // CONTACT FORM
-  // $('#contact-form').submit(function(e) {
-  //   e.preventDefault();
-
-  //     $.ajax({
-  //         url: "https://formspree.io/bl1723r@gmail.com",
-  //         method: "POST",
-  //         data: { message: $('form').serialize() },
-  //         dataType: "json"
-  //     }).done(function() {
-  //         $('#success').addClass('expand');
-  //         $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-  //     });
-  // });
-
-  // $('#close').click(function() {
-  //   $('#success').removeClass('expand');
-  // })
-
 });
 
 
